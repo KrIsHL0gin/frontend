46695dd939b1339250bb794c7c67db0d2753676b
KrIsHL0gin <KrIsHL0gin@users.noreply.github.com>
2024-10-01 20:26:47 +0530

    Food-ordering

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo3} alt="" className='logo'/>
+            <p></p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>History</li>
+                <li>Private policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>Contact no.</li>
+                <li>Email@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 Bytes&Bite - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        
+        <h2>Order your Favourite food here </h2>
+          <p>Food is the substance we eat every day for energy and strength. 
+            There are many different types of food, such as fruits, vegetables, rice, and pasta. 
+            We need to eat a variety of foods to get all the essential nutrients the body needs.
+            Not eating a healthy and balanced diet leads to weakness and deficiency diseases.</p>
+          <button>view Menu</button>
+        </div>
+      </div>
+      )
+}
+      export default Header
